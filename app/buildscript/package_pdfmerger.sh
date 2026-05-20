1b5a331a9afc38314125a30191b3ad4f75954f81
xiaobo <xiaobo.j.jiang@gmail.com>
2026-05-20 15:05:47 +0800

    add build script for macos application

---
+++ app/buildscript/package_pdfmerger.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python -m PyInstaller pdfmerger.spec
+
+
+#note, do this first: source venv/bin/activate
---
+++ app/buildscript/package_serialmd.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python -m PyInstaller serialdebuggerkivymd.spec
+
+#note, do this first: source venv/bin/activate
